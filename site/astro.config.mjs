d8c743003ceda4d0b7f309fc4b336f6c255d9c7f
sorcererxw <tujipqy@gmail.com>
2026-04-19 21:35:44 +0800

    Add Cloudflare Worker deployment config for site

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: "https://hopter.dev",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
   adapter: cloudflare(),
-  output: "static",
 });
